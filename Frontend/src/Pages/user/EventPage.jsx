5ec4123bdd3e7e8a49cbf3047138ff0828620008
Gokul-nath-K <gokulnathk003@gmail.com>
2024-03-18 15:59:24 +0530

    Update EventPage.jsx

--- Frontend/src/Pages/user/EventPage.jsx
+++ Frontend/src/Pages/user/EventPage.jsx
@@ -5,7 +5,7 @@ import { EventConstants } from "../../constants/EventConstants";
 const EventPage = ({ length = EventConstants.length }) => {
   return (
     <>
-      <div className="bg-white relative my-[1%]">
+      <div className="bg-white relative">
         <div className="flex justify-center py-5 ">
           /<p className="text-[35px]">OUR SERVICES</p>
         </div>
