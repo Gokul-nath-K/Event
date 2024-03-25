f52d3eb050924b1758abd1182a73d16c71e2ab7b
Gokul-nath-K <gokulnathk003@gmail.com>
2024-03-25 19:02:44 +0530

    Backend project created

---
+++ Backend/Golden_Glitz/src/main/java/limited_edition/Golden_Glitz/GoldenGlitzApplication.java
@@ -0,0 +1,13 @@
+package limited_edition.Golden_Glitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenGlitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenGlitzApplication.class, args);
+	}
+
+}
---
+++ Backend/Golden_Glitz/src/test/java/limited_edition/Golden_Glitz/GoldenGlitzApplicationTests.java
@@ -0,0 +1,13 @@
+package limited_edition.Golden_Glitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenGlitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
